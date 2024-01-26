04c523392f805eb5eddbfe5babb0afe58331d744
Yury Chebotaev <yury.79120345101@gmail.com>
2024-01-26 20:53:02 +0500

    Support for postgres in pincodes-service

--- pincodes-service/knexfile.ts
+++ pincodes-service/knexfile.ts
@@ -1,9 +1,28 @@
-import path from 'node:path'
-import { type Knex } from 'knex'
+import path from "node:path";
+import { type Knex } from "knex";
 
-export default {
-  client: 'sqlite3',
-  connection: {
-    filename: path.resolve(__dirname, 'db.sqlite3')
-  }
-} satisfies Knex.Config
+const user = process.env["PG_USER"];
+const password = process.env["PG_PASSWORD"];
+
+let config: Knex.Config;
+
+if (user && password) {
+  config = {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      user,
+      password,
+      database: "pincodes",
+    },
+  } satisfies Knex.Config;
+} else {
+  config = {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(__dirname, "db.sqlite3"),
+    },
+  } satisfies Knex.Config;
+}
+
+export default config;
