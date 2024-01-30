8c809003920b2e73eb94d883ba754f4631364828
Yury Chebotaev <yury.79120345101@gmail.com>
2024-01-30 16:06:49 +0500

    Parse pincode payload only if it's string

--- pincodes-service/lib/persistence/pinCodes.ts
+++ pincodes-service/lib/persistence/pinCodes.ts
@@ -1,16 +1,16 @@
-import { knex } from '../knex'
-import type { PinCode } from './types'
+import { knex } from "../knex";
+import type { PinCode } from "./types";
 
 export const pinCodeCreate = async ({
   tenantId,
   generatorId,
   code,
-  payload
+  payload,
 }: {
-  tenantId: number
-  generatorId: number
-  code: string
-  payload: any
+  tenantId: number;
+  generatorId: number;
+  code: string;
+  payload: any;
 }) => {
   const [{ id }] = await knex
     .insert({
@@ -18,28 +18,31 @@ export const pinCodeCreate = async ({
       generatorId,
       code,
       payload: JSON.stringify(payload),
-      createdAt: new Date().getTime()
+      createdAt: new Date().getTime(),
     })
-    .into('pinCodes')
-    .returning<{ id: number }[]>('id')
+    .into("pinCodes")
+    .returning<{ id: number }[]>("id");
 
-  return id
-}
+  return id;
+};
 
 export const pinCodeGetByCode = async (tenantId: number, code: string) => {
   const pinCode = await knex
-    .select('*')
-    .from('pinCodes')
-    .where('code', code)
-    .andWhere('tenantId', tenantId)
-    .first()
+    .select("*")
+    .from("pinCodes")
+    .where("code", code)
+    .andWhere("tenantId", tenantId)
+    .first();
 
   if (!pinCode || pinCode.deleted) {
-    return
+    return;
   }
 
   return {
     ...pinCode,
-    payload: JSON.parse(pinCode.payload)
-  } as PinCode
-}
+    payload:
+      typeof pinCode.payload === "string"
+        ? JSON.parse(pinCode.payload)
+        : pinCode.payload,
+  } as PinCode;
+};
