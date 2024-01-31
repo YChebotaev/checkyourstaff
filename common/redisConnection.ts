f4033525cf19ca38c844219b5902d486fc4565f8
Yury Chebotaev <yury.79120345101@gmail.com>
2024-01-31 20:29:10 +0500

    Disable redis password

--- common/redisConnection.ts
+++ common/redisConnection.ts
@@ -2,6 +2,5 @@ import { Redis } from "ioredis";
 
 export const redisConnection = new Redis({
   port: 6379,
-  host: "localhost",
-  password: process.env["REDIS_PASSWORD"],
+  host: "localhost"
 });
