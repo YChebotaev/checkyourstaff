2e236cdbbcc93d63a73174c7560dfb28d7578c4f
ychebotaev <yury.79120345101@gmail.com>
2024-02-11 15:51:22 +0500

    Fix script err 2

--- scripts/force-start-poll-session.ts
+++ scripts/force-start-poll-session.ts
@@ -1,16 +1,16 @@
 import path from "node:path";
-import fs from "node:fs";
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import { createLogger } from "@checkyourstaff/common/createLogger";
-import { parse } from "dotenv";
+import { config } from "dotenv";
 import { spinPoll } from "@checkyourstaff/workers/lib/spinPoll";
 
 const logger = createLogger("force-start-poll-session");
 
-Object.assign(process.env, parse(
-  fs.readFileSync(path.resolve(__dirname, "../workers/.env"), "utf-8"),
-))
+config({
+  path: path.resolve(__dirname, "../workers/.env"),
+  encoding: 'utf-8'
+})
 
 const { pollId, sampleGroupId } = yargs(hideBin(process.argv))
   .option("poll-id", {
